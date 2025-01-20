599ae4da6694a09f985fa724ac2b72b67bb9b96f
nuno1alves <nuno.alves1@outlook.pt>
2025-01-20 20:13:53 +0000

    Update palavras.js

--- Jogo da Forca/recursos/palavras.js
+++ Jogo da Forca/recursos/palavras.js
@@ -2,9 +2,9 @@
 const palavrasEDicas = [
   { palavra: "JAVASCRIPT", dica: "Linguagem de programação usada na web" },
   { palavra: "GIRAFA", dica: "Animal com o pescoço longo" },
-  { palavra: "FUTEBOL", dica: "Esporte popular com uma bola" },
+  { palavra: "FUTEBOL", dica: "Desporto popular com uma bola" },
   { palavra: "COMPUTADOR", dica: "Dispositivo usado para processar dados" },
-  // Adicione mais palavras conforme necessário
+  
 ];
 
   
