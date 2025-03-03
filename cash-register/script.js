b414bf0a69b08db72e20ffbe11fa2129b86a859e
nuno1alves <nuno.alves1@outlook.pt>
2025-03-03 14:53:20 +0000

    update projeto

--- cash-register/script.js
+++ cash-register/script.js
@@ -12,5 +12,28 @@ let cid = [
 ];
 
 const screenPrice = document.getElementById("screen-price");
+const changeInDrawer = document.getElementById("change-in-drawer");
+
+//teste
+const cash = {
+  'Pennies': 1.01,
+  'Nickels': 2.05,
+  'Dimes': 3.1,
+  'Quarters': 4.25,
+  'Ones': 90,
+  'Fives': 55,
+  'Tens': 20,
+  'Twenties': 60,
+  'Hundreds': 100,
+};
 
 screenPrice.textContent = `Total: $${price}`;
+
+updateUI = () => {
+  changeInDrawer.innerHTML = `<p><strong>Change in drawer:</strong></p>`;
+  Object.entries(cash).forEach(([key, val]) => {
+    changeInDrawer.innerHTML += `<p>${key}: $${val}</p>`;
+});
+};
+
+updateUI();
